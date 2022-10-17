b6e04ec0b1dccb4c0d0ec1002e1dc2213c8ca999
arnottrcaiado <icapp@8e86bfb0e784>
2022-10-17 23:17:25 +0000

    eliminando linhas sem uso

--- mysite/flask_app_back.py
+++ mysite/flask_app_back.py
@@ -141,3 +141,83 @@ def salva_log( arquivo, ipuser) :
     hora_atual = hora_atual[0:5]
     arq.write(data_atual+";"+hora_atual+";"+ipuser+"\n")
     arq.close()
+    return
+'''
+# #### APIs ###########
+# Selecionar Tudo
+@app.route("/usuarios", methods=["GET"])
+def seleciona_usuarios():
+    usuarios_objetos = Usuario.query.all()
+    usuarios_json = [usuario.to_json() for usuario in usuarios_objetos]
+
+    return gera_response(200, "usuarios", usuarios_json)
+
+# Selecionar Individual
+@app.route("/usuario/<id>", methods=["GET"])
+def seleciona_usuario(id):
+    usuario_objeto = Usuario.query.filter_by(id=id).first()
+    usuario_json = usuario_objeto.to_json()
+
+    return gera_response(200, "usuario", usuario_json)
+
+# Cadastrar
+# https://icapp.pythonanywhere.com/usuario
+@app.route("/usuario", methods=["POST"])
+def cria_usuario():
+    body = request.get_json()
+
+    try:
+        usuario = Usuario(nome=body["nome"], email= body["email"])
+        db.session.add(usuario)
+        db.session.commit()
+        return gera_response(201, "usuario", usuario.to_json(), "Criado com sucesso")
+    except Exception as e:
+        print('Erro', e)
+        return gera_response(400, "usuario", {}, "Erro ao cadastrar")
+
+
+# Atualizar
+# https://icapp.pythonanywhere.com/usuario/<id>
+@app.route("/usuario/<id>", methods=["PUT"])
+def atualiza_usuario(id):
+    usuario_objeto = Usuario.query.filter_by(id=id).first()
+    body = request.get_json()
+
+    try:
+        if('nome' in body):
+            usuario_objeto.nome = body['nome']
+        if('email' in body):
+            usuario_objeto.email = body['email']
+
+        db.session.add(usuario_objeto)
+        db.session.commit()
+        return gera_response(200, "usuario", usuario_objeto.to_json(), "Atualizado com sucesso")
+    except Exception as e:
+        print('Erro', e)
+        return gera_response(400, "usuario", {}, "Erro ao atualizar")
+
+# Deletar
+# https://icapp.pythonanywhere.com/usuario/<id>
+@app.route("/usuario/<id>", methods=["DELETE"])
+def deleta_usuario(id):
+    usuario_objeto = Usuario.query.filter_by(id=id).first()
+
+    try:
+        db.session.delete(usuario_objeto)
+        db.session.commit()
+        return gera_response(200, "usuario", usuario_objeto.to_json(), "Deletado com sucesso")
+    except Exception as e:
+        print('Erro', e)
+        return gera_response(400, "usuario", {}, "Erro ao deletar")
+
+
+def gera_response(status, nome_do_conteudo, conteudo, mensagem=False):
+    body = {}
+    body[nome_do_conteudo] = conteudo
+
+    if(mensagem):
+        body["mensagem"] = mensagem
+
+    return Response(json.dumps(body), status=status, mimetype="application/json")
+'''
+
